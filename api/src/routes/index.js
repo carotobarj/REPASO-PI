a64132c0795276e016963ea642745f68610f608a
Caro Tobar J <carotobarj@gmail.com>
2022-05-04 15:33:34 -0500

    back finalizado

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -13,42 +13,73 @@ const allCharactersApi = async (req, res) => {
                 species: el.species,
                 origin: el.origin.name,
                 image: el.image,
-                episode: el.episode.map((e) => e.name)
+                episode: el.episode
             }
         })
         return characterApi;
-    
+
         console.log(characterApi)
-        
+
     } catch (error) {
         console.log(error);
     }
 }
 
-const getAllEpisodes = async () =>{
+const getAllEpisodes = async () => {
     let episodesApi = await axios.get(`https://rickandmortyapi.com/api/episode`)
-    let episodeApi = await episodesApi.data.results.map((el)=>{
-        return{
+    let episodeApi = await episodesApi.data.results.map((el) => {
+        return {
             id: el.id,
             name: el.name,
         }
-     })
+    })
     return episodeApi
 }
 
-const getAllInfo = async ()=>{
+const getAllInfo = async () => {
     let characterInfo = await allCharactersApi();
     let episodeInfo = await getAllEpisodes();
     let infoTotal = characterInfo.concat(episodeInfo);
     return infoTotal;
 
 }
-router.get("/character", async (req, res) => {
+router.get("/characters", async (req, res) => {
     const charactersApi = await getAllInfo();
     res.json(charactersApi);
-   
-})
+});
 
+router.get("/episodes", async (req, res) => {
+    const episodesApi = await getAllEpisodes();
+        episodesApi.forEach(el => {
+        Episode.findOrCreate({
+            where: {name: el.name}
+        })
+      });
+      const totalEpisodes = await Episode.findAll();
+    res.json(totalEpisodes);
+
+});
+router.post("/character", async(req, res) =>{
+    const { name, species, origin, image, episode} =req.body;
+    const newCharacter = await Character.create({name, species, origin, image, episode })
+    let episodebyCharacter = await Episode.findAll({
+        where: {name: episode}
+    })
+newCharacter.addEpisode(episodebyCharacter);
+return res.send("personaje creado exitosamente!") 
+})
+// consultar como arreglar el delete
+router.delete("/character", async(req, res) =>{
+    try{
+        const {id} = req.params;
+        res.json(await Character.destroy({
+             where: {id} 
+        }))
+     } catch(error){
+         res.send(error)
+     }
+       
+})
 
 // Configurar los routers
 
